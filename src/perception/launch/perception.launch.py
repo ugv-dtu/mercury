886a8db2a91467607b33ee80207be1f7720316a9
dakshhpanchal <dakshpanchal08@gmail.com>
2026-03-23 16:33:23 +0530

    made progess in lane

--- src/perception/launch/perception.launch.py
+++ src/perception/launch/perception.launch.py
@@ -15,7 +15,7 @@ def generate_launch_description():
                 "image_topic": "/camera/image_raw",
                 "camera_info_topic": "/camera/camera_info",
                 "costmap_topic": "/perception/road_costmap",
-                "costmap_frame": "map",            # ← also fix this (was "base_link")
+                "costmap_frame": "odom",            # ← also fix this (was "base_link")
                 "bev_width": 800,
                 "bev_height": 600,
                 "resolution": 0.0051,
